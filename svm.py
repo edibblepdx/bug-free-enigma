b059aba33419698c36043f9fd7665198740dd037
ethan <ethanrdibble@gmail.com>
2024-08-16 06:39:22 -0700

    csv

--- svm.py
+++ svm.py
@@ -1,3 +1,6 @@
+# Ethan Dibble
+# SVM classifier using CNN feature extraction
+
 from FeatureExtract import FeatureExtract
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
