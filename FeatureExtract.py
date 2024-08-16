00864981ba2ab1e6a9ff9fb50eb6a86781bdcb0a
ethan <ethanrdibble@gmail.com>
2024-08-16 05:12:20 -0700

    testing

--- FeatureExtract.py
+++ FeatureExtract.py
@@ -12,6 +12,7 @@ from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import Conv1D, Dropout, Flatten, MaxPooling1D, Dense
 from keras.saving import load_model
+from keras import Model
 import matplotlib.pyplot as plt
 import argparse
 import sys
@@ -29,8 +30,8 @@ class FeatureExtract:
         data_path should be something like Data/genres_original
         features_path should be something like Data/features_30_sec.csv
         """
-        self.features = []
-        self.labels = []
+        features = []
+        labels = []
 
         data_features = pd.read_csv(features_path)
 
@@ -51,59 +52,70 @@ class FeatureExtract:
                 # should also simplify features
 
                 # append features and labels
-                self.features.append(mfccs_scaled)
-                self.labels.append(row['label'])
+                features.append(mfccs_scaled)
+                labels.append(row['label'])
  
             except Exception as e:
-                sys.stderr.write(f"Error processing {file_path}: {e}")
+                sys.stderr.write(f"Error processing {file_path}: {e}\n")
 
-        # convert to numpy arrays
-        self.features = np.array(self.features)
-        self.labels = np.array(self.labels)
+        return np.array(features), np.array(labels)
 
-    def train(self, predict=False):
+    def train(self, features, labels, predict=False):
         """train the cnn"""
-        labels_onehot, label_encoder = self.__preprocess_labels()
-        self.model_full = self.__build_cnn(len(label_encoder.classes_))
-        x_train, x_test, y_train, y_test = train_test_split(self.features, labels_onehot, test_size=0.2, random_state=42, stratify=labels_onehot)
+        labels_onehot, label_encoder = self.__preprocess_labels(labels)
+        self.model = self.__build_cnn(features, len(label_encoder.classes_))
+        x_train, x_test, y_train, y_test = train_test_split(features, labels_onehot, test_size=0.2, random_state=42, stratify=labels_onehot)
 
-        self.model_full.fit(x_train, y_train, epochs=20, validation_data=(x_test, y_test), verbose=1)
-        self.model = self.model_full.layers[:-2]
+        self.model.fit(x_train, y_train, epochs=20, validation_data=(x_test, y_test), verbose=1)
+        self.feature_extractor = Model(
+            inputs=self.model.inputs,
+            outputs=self.model.get_layer(name="stop").output,
+            #outputs=[layer.output for layer in self.model.layers[:-4]],
+        )
 
         if predict:
             self.__predict(x_test, y_test, label_encoder)
 
+        #self.model.pop()
+        #self.model.pop()
+
     def extract(self, input):
         """extract features from input"""
         try:
-            return self.model.predict(input)
+            # https://keras.io/guides/sequential_model/#feature-extraction-with-a-sequential-model
+            # print(type(self.feature_extractor(input)))
+            return self.feature_extractor(input)
 
         except Exception as e:
-            sys.stderr.write(f"Error extracting features: {e}")
-            sys.stderr.write("train or load the model first")
+            sys.stderr.write(f"Error extracting features: {e}\n")
+            sys.stderr.write("train or load the model first\n")
 
     def save(self, path=None, overwrite=False):
         """save the cnn"""
         if path:
-            self.model_full.save(path, overwrite=overwrite)
+            self.model.save(path, overwrite=overwrite)
 
         else:
-            self.model_full.save("./cnn.keras", overwrite=overwrite)
+            self.model.save("./cnn.keras", overwrite=overwrite)
 
     def load(self, path):
         """load the cnn"""
         try:
-            self.model_full = load_model(path)
-            self.model = self.model_full[:-2]
+            self.model = load_model(path)
+            self.feature_extractor = Model(
+                inputs=self.model.inputs,
+                outputs=self.model.get_layer(name="stop").output,
+                #outputs=[layer.output for layer in self.model.layers[:-4]],
+            )
 
         except Exception as e:
-            sys.stderr.write(f"Error loading model: {e}")
+            sys.stderr.write(f"Error loading model: {e}\n")
 
     def __predict(self, x_test, y_test, label_encoder):
         """testing only"""
         try:
             # predictions
-            predictions = self.model_full.predict(x_test) # shape(num_inputs, num_classes)
+            predictions = self.model.predict(x_test) # shape(num_inputs, num_classes)
             predicted_classes = np.argmax(predictions, axis=1) # largest indices
             predicted_labels = label_encoder.inverse_transform(predicted_classes)
             true_classes = np.argmax(y_test, axis=1)
@@ -122,83 +134,52 @@ class FeatureExtract:
             print (f"accuracy: {accuracy}")
         
         except Exception as e:
-            sys.stderr.write(f"Error in prediction: {e}")
+            sys.stderr.write(f"Error in prediction: {e}\n")
 
-    def __preprocess_labels(self):
+    def __preprocess_labels(self, labels):
         """one-hot encode categorical labels"""
         label_encoder = LabelEncoder()
-        labels_encoded = label_encoder.fit_transform(self.labels) # returns an array of labels converted to integers
+        labels_encoded = label_encoder.fit_transform(labels) # returns an array of labels converted to integers
         labels_onehot = to_categorical(labels_encoded) # returns an array of one-hot encoded vector labels
 
         return labels_onehot, label_encoder
 
-    def __build_cnn(self, num_classes):
+    def __build_cnn(self, features, num_classes):
         """Create a cnn model for feature extraction with input, convolutional, pooling layers"""
-        input_shape = (self.features.shape[1], 1)
-        model_full = Sequential([
-            Conv1D(64, 3, padding='same', activation='relu', input_shape=input_shape)
+        input_shape = (features.shape[1], 1)
+        model = Sequential([
+            #Conv1D(64, 3, padding='same', activation='relu', input_shape=input_shape)
+            #, MaxPooling1D(pool_size=2)
+            #, Dropout(0.25)
+            #, Conv1D(128, 3, padding='same', activation='relu')
+            #, MaxPooling1D(pool_size=2)
+            #, Dropout(0.25)
+            #, Flatten()
+            #, Dense(512, activation='relu')
+            #, Dropout(0.5)
+            #, Dense(num_classes, activation='softmax')
+            Conv1D(32, 3, padding='same', activation='relu', input_shape=(features.shape[1], 1))
+            , Conv1D(32, 3, padding='same', activation='relu')
             , MaxPooling1D(pool_size=2)
-            , Dropout(0.25)
-            , Conv1D(128, 3, padding='same', activation='relu')
-            , MaxPooling1D(pool_size=2)
-            , Dropout(0.25)
+            , Conv1D(32, 3, padding='same', activation='relu')
+            , Conv1D(32, 3, padding='same', activation='relu')
+            , MaxPooling1D(pool_size=2, name='stop')
             , Flatten()
-            , Dense(512, activation='relu')
+            , Dense(64, activation='relu')
             , Dropout(0.5)
             , Dense(num_classes, activation='softmax')
         ])
-        model_full.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-        return model_full
+        return model
 
 #def main(inputs, labels, output, save):
 def main():
     """example use"""
-    """
-    features, labels = load_data('Data/genres_original', 'Data/features_30_sec.csv')
-    features = features.reshape(features.shape[0], features.shape[1], 1) #!
-    cnn_model = create_cnn(features)
-
-    extracted_features = cnn_model.predict(features)
-
-    # what follows is a training example with a dense neural network
-
-    # one-hot labels and splitting sets
-    labels_onehot, label_encoder = preprocess_labels(labels)
-    x_train, x_test, y_train, y_test = train_test_split(extracted_features, labels_onehot, test_size=0.2, random_state=42, stratify=labels_onehot)
-
-    # create model
-    dnn_model = Sequential([
-        Dense(512, activation='relu', input_shape=(x_train.shape[1],))
-        , Dropout(0.5)
-        , Dense(len(label_encoder.classes_), activation='softmax')
-    ])
-    dnn_model = Sequential([
-        Dense(256, activation='relu', input_shape=(x_train.shape[1],))
-        , Dense(128, activation='relu')
-        , Dense(64, activation='relu')
-        , Dense(10, activation='softmax')
-    ])
-    # https://keras.io/api/optimizers/
-    dnn_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-    # training
-    # history = dnn_model.fit(x_train, y_train, epochs=20, validation_data=(x_test, y_test), verbose=1)
-    history = dnn_model.fit(x_train, y_train, epochs=20, batch_size=128)
-    print(history.history)
-
-    # predictions
-    predictions = dnn_model.predict(x_test) # shape(num_inputs, num_classes)
-    predicted_classes = np.argmax(predictions, axis=1) # largest indices
-    predicted_labels = label_encoder.inverse_transform(predicted_classes)
-    true_classes = np.argmax(y_test, axis=1)
-    true_labels = label_encoder.inverse_transform(true_classes)
-    """
-
     fe = FeatureExtract()
-    fe.load_data('Data/genres_original', 'Data/features_30_sec.csv')
-    fe.train(predict=True)
-    fe.save()
+    features, labels = fe.load_data('Data/genres_original', 'Data/features_30_sec.csv')
+    fe.train(features, labels, predict=True)
+    fe.save(path='cnn3.keras', overwrite=True)
 
 if __name__ == '__main__':
     """
---
+++ svm.py
@@ -0,0 +1,58 @@
+from FeatureExtract import FeatureExtract
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn import svm
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    fe = FeatureExtract()
+    fe.load('cnn3.keras')
+    x, y = fe.load_data('Data/genres_original', 'Data/features_30_sec.csv')
+
+    #print(fe.model.summary)
+    #print(fe.feature_extractor.summary)
+
+    features = fe.extract(x).numpy()
+    print(type(features))
+    print(features.shape)
+    features = features.reshape(999,-1)
+    print(features.shape)
+
+    label_encoder = LabelEncoder()
+    labels_encoded = label_encoder.fit_transform(y)
+    #labels_onehot = to_categorical(labels_encoded)
+
+    x_train, x_test, y_train, y_test = train_test_split(features, labels_encoded, test_size=0.2, random_state=42, stratify=labels_encoded)
+
+    clf = svm.SVC()
+    clf.fit(x_train, y_train)
+    predictions = clf.predict(x_test)
+
+    #predicted_classes = np.argmax(predictions, axis=1) # largest indices
+    #predicted_labels = label_encoder.inverse_transform(predicted_classes)
+    #true_classes = np.argmax(y_test, axis=1)
+    #true_labels = label_encoder.inverse_transform(true_classes)
+
+    print(np.unique(y_test))
+    print(label_encoder.classes_)
+
+    # confusion matrix
+    predicted_labels = label_encoder.inverse_transform(predictions)
+    true_labels = label_encoder.inverse_transform(y_test)
+    cm = confusion_matrix(true_labels, predicted_labels, labels=label_encoder.classes_)
+    display = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=label_encoder.classes_)
+    display.plot(cmap=plt.cm.Blues)
+    plt.xticks(rotation=45)
+    plt.title('Confusion Matrix DNN')
+    plt.show()
+
+    # accuracy
+    accuracy = np.sum(predictions == y_test) / len(y_test)
+    print (f"accuracy: {accuracy}")
+ 
+
+if __name__ == '__main__':
+    main()
